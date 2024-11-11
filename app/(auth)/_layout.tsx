70a90136d4cd7e6d44275de5e65fe641b18d4a51
tony-alignnutrition <tony@align-nutrition.com>
2024-11-11 16:56:35 -0700

    Remove unused imports

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,7 @@
 import { UserProvider } from "@/contexts/user-context";
-import { supabase } from "@/lib/supabase";
 import { Redirect, Stack } from "expo-router";
-import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { useSession } from "../../contexts/auth-context";
-import { Tables } from "@/supabase";
 
 export default function AppLayout() {
   const { session, isLoading } = useSession();
