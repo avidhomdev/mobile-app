3390af866611310b526dcf9449d270e5b1375116
tony-alignnutrition <tony@thesavvy.dev>
2025-01-16 18:20:44 -0700

    Add friendly date and time formats

--- constants/date-formats.ts
+++ constants/date-formats.ts
@@ -1,2 +1,4 @@
 export const SERVER_DATE_TIME_FORMAT = "YYYY-MM-DD HH:mm";
 export const SHORT_FRIENDLY_DATE_TIME_FORMAT = "ddd, MMM D, YYYY hh:mm a";
+export const FRIENDLY_DATE_FORMAT = "ddd, MMM D, YYYY";
+export const TIME_FORMAT = "hh:mm a";
