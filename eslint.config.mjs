a7f69243d8e0f6a75c682e20952505b719cf2bc4
tony-alignnutrition <tony@thesavvy.dev>
2025-01-20 17:40:06 -0700

    Update eslint plugin and fix errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
   eslintConfigPrettier,
   {
     rules: {
