17c8a7eaa72d03f248ded8c1dc637fefcef65a67
tony <--global>
2025-04-28 08:41:10 -0600

    Update login screen

--- app/login.tsx
+++ app/login.tsx
@@ -10,12 +10,14 @@ import { Text } from "@/components/ui/text";
 import { useSession } from "@/contexts/auth-context";
 import { Redirect, router } from "expo-router";
 import { useState } from "react";
-import { KeyboardAvoidingView, View } from "react-native";
+import { KeyboardAvoidingView } from "react-native";
 
-import { Button, ButtonText } from "@/components/ui/button";
-import { Input, InputField } from "@/components/ui/input";
-import { AlertCircleIcon } from "lucide-react-native";
 import { Alert, AlertText } from "@/components/ui/alert";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Input, InputField } from "@/components/ui/input";
+import { VStack } from "@/components/ui/vstack";
+import { AlertCircleIcon } from "lucide-react-native";
 
 export default function Login() {
   const { signIn, session } = useSession();
@@ -34,61 +36,66 @@ export default function Login() {
   }
 
   return (
-    <View className="flex-1 justify-center p-4 gap-y-6 bg-white">
-      <View>
-        <Text className="text-center" size="3xl">
+    <VStack
+      className="bg-background-light dark:bg-background-dark flex-1 justify-center px-6"
+      space="lg"
+    >
+      <VStack space="xs">
+        <Heading className="text-typography-800 text-center" size="3xl">
           Welcome back!
-        </Text>
-        <Text>
+        </Heading>
+        <Text className="text-typography-500 text-center">
           Sign in to view your jobs, manage your schedule and start tracking
           time.
         </Text>
-      </View>
-      <KeyboardAvoidingView behavior="padding" className="gap-y-6">
-        {error && (
-          <Alert action="error">
-            <AlertText>{error}</AlertText>
-          </Alert>
-        )}
-        <FormControl isRequired>
-          <FormControlLabel>
-            <FormControlLabelText>Email</FormControlLabelText>
-          </FormControlLabel>
-          <Input variant="outline" size="lg">
-            <InputField
-              autoComplete="off"
-              autoCapitalize="none"
-              autoCorrect={false}
-              onChangeText={setEmail}
-              placeholder="johndoe@exmaple.com"
-              value={email}
-            />
-          </Input>
-          <FormControlError>
-            <FormControlErrorIcon as={AlertCircleIcon} />
-            <FormControlErrorText>Email is required</FormControlErrorText>
-          </FormControlError>
-        </FormControl>
-        <FormControl isRequired>
-          <FormControlLabel>
-            <FormControlLabelText>Password</FormControlLabelText>
-          </FormControlLabel>
-          <Input variant="outline" size="lg">
-            <InputField
-              onChangeText={setPassword}
-              type="password"
-              value={password}
-            />
-          </Input>
-          <FormControlError>
-            <FormControlErrorIcon as={AlertCircleIcon} />
-            <FormControlErrorText>Password is required</FormControlErrorText>
-          </FormControlError>
-        </FormControl>
-        <Button onPress={handleLogin}>
-          <ButtonText className="text-center font-bold">Sign in</ButtonText>
-        </Button>
+      </VStack>
+      <KeyboardAvoidingView behavior="padding">
+        <VStack space="lg">
+          {error && (
+            <Alert action="error">
+              <AlertText>{error}</AlertText>
+            </Alert>
+          )}
+          <FormControl isRequired>
+            <FormControlLabel>
+              <FormControlLabelText>Email</FormControlLabelText>
+            </FormControlLabel>
+            <Input variant="outline" size="lg">
+              <InputField
+                autoComplete="off"
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={setEmail}
+                placeholder="johndoe@exmaple.com"
+                value={email}
+              />
+            </Input>
+            <FormControlError>
+              <FormControlErrorIcon as={AlertCircleIcon} />
+              <FormControlErrorText>Email is required</FormControlErrorText>
+            </FormControlError>
+          </FormControl>
+          <FormControl isRequired>
+            <FormControlLabel>
+              <FormControlLabelText>Password</FormControlLabelText>
+            </FormControlLabel>
+            <Input variant="outline" size="lg">
+              <InputField
+                onChangeText={setPassword}
+                type="password"
+                value={password}
+              />
+            </Input>
+            <FormControlError>
+              <FormControlErrorIcon as={AlertCircleIcon} />
+              <FormControlErrorText>Password is required</FormControlErrorText>
+            </FormControlError>
+          </FormControl>
+          <Button onPress={handleLogin}>
+            <ButtonText className="text-center font-bold">Sign in</ButtonText>
+          </Button>
+        </VStack>
       </KeyboardAvoidingView>
-    </View>
+    </VStack>
   );
 }
