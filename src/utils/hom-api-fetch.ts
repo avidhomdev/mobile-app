78e06ade533c5f6594db2dc38444f6a4c483f1b4
tony <--global>
2025-06-14 07:14:31 -0600

    Update homFetchApi header override pattern

--- src/utils/hom-api-fetch.ts
+++ src/utils/hom-api-fetch.ts
@@ -3,9 +3,11 @@ import { supabase } from "@/src/lib/supabase";
 export async function homApiFetch({
   endpoint,
   options,
+  responseType = "json",
 }: {
   endpoint: string;
   options: RequestInit;
+  responseType?: "json" | "text" | "blob";
 }) {
   if (endpoint.startsWith("/")) {
     throw new Error("Endpoint must not start with /");
@@ -16,13 +18,14 @@ export async function homApiFetch({
   } = await supabase.auth.getSession();
 
   return fetch(`${process.env.EXPO_PUBLIC_HOM_API_URL}/${endpoint}`, {
-    headers: {
-      Authorization: `Bearer ${session?.access_token}`,
-      "Content-Type": "application/json",
-    },
     ...options,
+    headers: {
+      ...options.headers,
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${session?.access_token}`,
+    },
   })
-    .then((res) => res.json())
+    .then((res) => res[responseType]())
     .then((res) => {
       if (res.error) throw new Error(res.error);
       return res;
