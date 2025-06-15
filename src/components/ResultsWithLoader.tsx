2a6d0384c1afc03e0d3f58d16988cf569422dcea
tony <--global>
2025-06-15 09:06:48 -0600

    Add results with loader component

---
+++ src/components/ResultsWithLoader.tsx
@@ -0,0 +1,27 @@
+import { PropsWithChildren } from "react";
+import { Text } from "./ui/text";
+import { ActivityIndicator } from "react-native";
+
+type ResultsWithLoaderProps = PropsWithChildren<{
+  isFetching: boolean;
+  hasResults: boolean;
+}>;
+
+function Results({
+  children,
+  hasResults,
+}: PropsWithChildren<{ hasResults: boolean }>) {
+  return hasResults ? children : <Text>No results</Text>;
+}
+
+export function ResultsWithLoader({
+  children,
+  isFetching,
+  hasResults,
+}: ResultsWithLoaderProps) {
+  return isFetching ? (
+    <ActivityIndicator />
+  ) : (
+    <Results hasResults={hasResults}>{children}</Results>
+  );
+}
